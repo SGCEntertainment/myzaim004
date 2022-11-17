16a47efa835b252181d34ff56a53305749fb6c03
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-11-17 20:11:32 +0300

    update

--- Assets/myzaim004/Scripts/Managers/AppManager.cs
+++ Assets/myzaim004/Scripts/Managers/AppManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class AppManager : MonoBehaviour
 {
@@ -20,11 +21,27 @@ public class AppManager : MonoBehaviour
     private IEnumerator InitMenu()
     {
         MenuItem menuItemPrefab = Resources.Load<MenuItem>("UI/menuItem");
-        Transform menuItemParent = GameObject.Find("menuItemParent").transform;
+        Transform menuItemParent = GameObject.Find("contentParent").transform;
 
         foreach(string folder in contentFolders)
         {
-            Instantiate(menuItemPrefab, menuItemParent).SetData(folder);
+            Instantiate(menuItemPrefab, menuItemParent).SetData(folder, () =>
+            {
+                StartCoroutine(InitFolderItems(Resources.LoadAll<TextAsset>($"Content/{folder}")));
+            });
+
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+
+    private IEnumerator InitFolderItems(TextAsset[] files)
+    {
+        MenuItem menuItemPrefab = Resources.Load<MenuItem>("UI/menuItem");
+        Transform menuItemParent = GameObject.Find("menuItemParent").transform;
+
+        foreach (TextAsset textAsset in files)
+        {
+            //Instantiate(menuItemPrefab, menuItemParent).SetData(folder);
             yield return new WaitForSeconds(0.25f);
         }
     }
--- Assets/myzaim004/Scripts/UI/MenuItem.cs
+++ Assets/myzaim004/Scripts/UI/MenuItem.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class MenuItem : MonoBehaviour
 {
-    public void SetData(string _name)
+    public void SetData(string _name, UnityEngine.Events.UnityAction OnPressAction)
     {
         GetComponentInChildren<Text>().text = _name;
+        GetComponentInChildren<Button>().onClick.AddListener(OnPressAction);
     }
 }
